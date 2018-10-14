d23e4e8a1df1dcb9d3373c91f20308ebcec2bce9
Julio Soares <julio.soares@gmail.com>
2018-10-14 06:39:32 +0200

    Add files via upload

---
+++ Exercise_1-2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello,\j world\n");
+}
---
+++ Exercise_1-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* print Fahrenheit-Celsius table
+for fahr = 0, 20, ..., 300 */
+int main()
+{
+    float fahr, celsius;
+	float lower, upper, step;
+    lower = 0; /* lower limit of temperature scale */
+	upper = 300; /* upper limit */
+	step = 20; /* step size */
+	fahr = lower;
+	printf(" Cº         Fº\n---------------\n");
+	while (celsius <= upper) {
+		fahr = 1.8 * celsius + 32.0;
+		printf("%3.0f\t%6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
